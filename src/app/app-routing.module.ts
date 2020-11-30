c20efd0920ae511ab4d8109d55446481b8859b4c
pooju07 <43408053+pooju07@users.noreply.github.com>
2020-11-30 09:38:50 +0530

    app navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import {HomeComponent} from './components/home/home.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path:'**',component:HomeComponent
+  }
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
+  providers:[],
+  bootstrap:[],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
